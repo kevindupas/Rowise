9c66cb0bbc260461b4107be1f8df446a5480ff57
Kévin Dupas <38907117+kevindupas@users.noreply.github.com>
2026-05-08 19:04:32 +0200

    fix: stop closing tabs on connection switch

--- src/lib/connect-to-database.ts
+++ src/lib/connect-to-database.ts
@@ -1,6 +1,5 @@
 import { connectDb, getPassword, getSshPassword, getServerVersion, getTlsInfo } from "./tauri-commands";
 import { useConnectionStore } from "../store/connections";
-import { useTabStore } from "../store/tabs";
 import type { Connection } from "../store/connections";
 
 export async function connectToDatabase(sourceConn: Connection, dbName: string): Promise<void> {
@@ -38,7 +37,6 @@ export async function connectToDatabase(sourceConn: Connection, dbName: string):
   }
 
   if (store.connectedIds.has(target.id)) {
-    useTabStore.getState().closeTabsForConnection(sourceConn.id);
     setActiveConnection(target.id);
     return;
   }
@@ -63,7 +61,6 @@ export async function connectToDatabase(sourceConn: Connection, dbName: string):
 
   await connectDb(config);
   setConnected(target.id, true);
-  useTabStore.getState().closeTabsForConnection(sourceConn.id);
 
   Promise.all([
     getServerVersion(config).catch(() => undefined),
